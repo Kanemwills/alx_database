b2f8c792447975a05be4947c5596c4213fb946ff
Kanemwills <udohprincewill144@gmail.com>
2023-08-17 01:04:57 +0100

    push all

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,2 +1,9 @@
-__ List all databases of my MySQL 
-SHOW DATABASES;
+#!/bin/bash
+
+# MySQL credentials
+MYSQL_USER="your_username"
+MYSQL_PASSWORD="your_password"
+
+# Execute the MySQL command to list databases
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;"
+
