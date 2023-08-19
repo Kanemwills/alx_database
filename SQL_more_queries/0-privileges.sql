8cd5d3c4a4850d25ae223113eef85e384d4e96d1
Kanemwills <udohprincewill144@gmail.com>
2023-08-19 01:21:09 +0100

    push

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Creates the user user_0d_2 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_2'@'localhost';
