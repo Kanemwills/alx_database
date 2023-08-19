d1bf528480a1a2f4c73734141824120997c2a909
Kanemwills <udohprincewill144@gmail.com>
2023-08-19 01:39:01 +0100

    push

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,8 @@
--- Creates the user user_0d_2 with all privileges.
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_2'@'localhost';
+   TO 'user_0d_1'@'localhost';
+   
